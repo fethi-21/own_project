8f4dfbbbc1fd4b5ec3786a57f1f0fdb46c6323c0
Fethi HATTAB <hattab.fethi21@gmail.com>
2023-07-16 00:14:36 +0200

    test_func_deleted

--- circle_class.py
+++ circle_class.py
@@ -8,12 +8,8 @@ class Cercle():
         return(math.pi*self.r**2 )
     def perim(self):
         return (2*math.pi*self.r)
-    def test(self,x,y):
-        if math.sqrt((x-self.a)**2+(y-self.b)**2)==self.r:
-            return True
-        else:
-            return False
+#test function deleted
 cer=Cercle(1,2,1)
 print(cer.surface())
 print(cer.perim())
-print(cer.test(1,1))
+
